8a3e8eb2080a3e5d3515afadccc36b9265c9c3e6
Lei Xu <lxu58@ucla.edu>
2023-02-07 17:39:50 -0800

    HW2

--- TMDB_scraper/TMDB_scraper/spiders/tmdb_spider.py
+++ TMDB_scraper/TMDB_scraper/spiders/tmdb_spider.py
@@ -5,6 +5,7 @@ import scrapy
 import requests
 from random import randint
 
+# https://scrapeops.io/docs/fake-user-agent-headers-api/integrations/python-requests/
 def get_user_agent_list():
     API = 'efccdead-3091-423f-952d-49f98106bbc0'
     response = requests.get('http://headers.scrapeops.io/v1/user-agents?api_key=' + API)
@@ -24,6 +25,7 @@ class TmdbSpider(scrapy.Spider):
         '''
             Request the main page of the movie and navigate to the movie page.
         '''
+        # the url starts on the movie page
         start_urls = ['https://www.themoviedb.org/tv/100088-the-last-of-us']
         headers = {'User-Agent': get_random_user_agent(user_agent_list)}
         for url in start_urls:
@@ -32,11 +34,15 @@ class TmdbSpider(scrapy.Spider):
 
     def parse(self, response):
         '''
-            Parse the movie page, and then navigate to the Cast & Crew page.
+            Parse the movie page, get the link to the Cast & Crew page
+            and then navigate to the Cast & Crew page.
         '''
+        # select the attribute href that navigates to the Cast & Crew page
         next_page = response.css('p.new_button a').attrib['href']
+        # get the whole link
         next_page = "https://www.themoviedb.org" + next_page
         headers = {'User-Agent': get_random_user_agent(user_agent_list)}
+        # yield a request to Cast & Crew url
         yield scrapy.Request(next_page, callback = self.parse_full_credits,headers=headers)
 
     def parse_full_credits(self, response):
@@ -44,8 +50,10 @@ class TmdbSpider(scrapy.Spider):
             Parse the Cast & Crew page, yield a scrapy.Request
             for the page of each actor listed on the page, and then navigate to the page of an actor.
         '''
+        # select the attribute href under cast
         link = response.css("section.panel.pad ol[class='people credits '] div.info p[class!='character'] a::attr(href)").getall()
         headers = {'User-Agent': get_random_user_agent(user_agent_list)}
+        # yield a request to each of actor's url
         for url in link:
             yield scrapy.Request(url = "https://www.themoviedb.org" + url, callback=self.parse_actor_page, headers=headers)
         
@@ -55,8 +63,11 @@ class TmdbSpider(scrapy.Spider):
             Parse the page of an actor, and output one such dictionary for each of the movies or TV shows
             on which that actor has worked. 
         '''
+        # get the actor's name
         actor_name = response.css("h2.title a::text").get()
+        # get the movie's or TV show's name
         movie_or_TV_name = response.css("div.credits_list table.credit_group a.tooltip bdi::text").getall()
+        # yield one dictionary for each of the movies or TV shows on which that actor has worked
         for movie in movie_or_TV_name:
             yield {"actor" : actor_name, 
                     "movie_or_TV_name" : movie}
